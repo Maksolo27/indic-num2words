1973f376e98bfcbcd07574734e0a8172cc20e1ce
Maksym <71383659+Maksolo27@users.noreply.github.com>
2022-04-28 15:13:23 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,4 +1,39 @@
-from setuptools import setup
-setup(
-    name="indic-num2words",
+import setuptools
+
+with open("README.md", "r") as fh:
+
+    long_description = fh.read()
+
+setuptools.setup(
+
+    name="<template-package-username>", # Replace with your username
+
+    version="1.0.0",
+
+    author="<authorname>",
+
+    author_email="<authorname@templatepackage.com>",
+
+    description="<Template Setup.py package>",
+
+    long_description=long_description,
+
+    long_description_content_type="text/markdown",
+
+    url="<https://github.com/Maksolo27/indic-num2words>",
+
+    packages=setuptools.find_packages(),
+
+    classifiers=[
+
+        "Programming Language :: Python :: 3",
+
+        "License :: OSI Approved :: MIT License",
+
+        "Operating System :: OS Independent",
+
+    ],
+
+    python_requires='>=3.6',
+
 )
